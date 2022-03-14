da2ab45cc2e50c657fc6f7abf2e26458b56dd4c7
LuisDiegoMora98 <aguilarluisdi@estudiantec.cr>
2022-03-14 03:21:08 -0600

    Cleaning failing project, wait for RB_2-Components fix

--- react-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-
-//JSX Hello World converts into a React.render thanks to Babel
-const element = <h1>Hello World</h1>;
-console.log(element);
-
-//This one moves the VirtualDOM into the Real DOM
-ReactDOM.render(element, document.getElementById('root'));
